adb111ca1fcc9b3af334b12598f6a3fc3f52e2f4
SurajMahato74 <surajmahato9823@gmail.com>
2025-10-13 13:47:15 +0545

    zxc

--- backend/ezeyway/ezeyway/urls.py
+++ backend/ezeyway/ezeyway/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.views.generic import TemplateView
-import os
 
 def ngrok_bypass_view(request):
     # Simple bypass for ngrok warning
@@ -13,21 +10,11 @@ def ngrok_bypass_view(request):
     response['ngrok-skip-browser-warning'] = 'any'
     return response
 
-def react_fallback_view(request):
-    # Serve React index.html from public_html
-    try:
-        with open('/home/ezeywayc/public_html/index.html', 'r') as f:
-            return HttpResponse(f.read(), content_type='text/html')
-    except FileNotFoundError:
-        return HttpResponse("React app not found", status=404)
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
-    path("api/", include("accounts.api_urls")),
+    path("", include("accounts.api_urls")),  # API at root since Passenger uses /api base
     path("ngrok-bypass/", ngrok_bypass_view, name='ngrok_bypass'),
-    # React fallback for all other routes
-    re_path(r'^.*$', react_fallback_view, name='react_fallback'),
 ]
 
 # Static files
