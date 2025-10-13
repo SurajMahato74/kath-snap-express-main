e9222028b2faa7bdd4535b34a88b987481f625ca
SurajMahato74 <surajmahato9823@gmail.com>
2025-10-13 08:47:54 +0545

    upadet

--- backend/ezeyway/ezeyway/urls.py
+++ backend/ezeyway/ezeyway/urls.py
@@ -1,17 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.shortcuts import render
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import HttpResponse
-import os
-
-def react_app_view(request, path=''):
-    try:
-        with open(os.path.join(settings.BASE_DIR, 'dist', 'index.html'), 'r') as f:
-            return HttpResponse(f.read(), content_type='text/html')
-    except FileNotFoundError:
-        return HttpResponse("index.html not found", status=404)
 
 def ngrok_bypass_view(request):
     # Simple bypass for ngrok warning
@@ -27,13 +18,9 @@ urlpatterns = [
     path("", include("accounts.api_urls")),  # Include API URLs at root level for backward compatibility
 ]
 
-# Static files (MUST come before catch-all)
+# Static files
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# Catch-all for React routes (MUST be last)
-urlpatterns += [re_path(r'^.*$', react_app_view, name='react_app')]
