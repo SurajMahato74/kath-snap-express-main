1c4a13113bccdf0cdb73861e3c6918c0ececfd07
SurajMahato74 <surajmahato9823@gmail.com>
2025-11-09 20:43:15 +0545

    updated

--- backend/ezeyway/accounts/order_views.py
+++ backend/ezeyway/accounts/order_views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 from django.utils import timezone
-from django.db.models import Q, Count, Sum, Avg
+from django.db.models import Q, Count, Sum, Avg, F
 from django.shortcuts import get_object_or_404
 from datetime import datetime, timedelta
 from .order_models import (
