c7dcd3c09aa164ac4271b1e1a4d28f57f699f997
SurajMahato74 <surajmahato9823@gmail.com>
2026-01-19 22:12:25 +0545

    udpaetd

--- backend/ezeyway/accounts/consumers.py
+++ backend/ezeyway/accounts/consumers.py
@@ -721,6 +721,35 @@ class CallConsumer(AsyncWebsocketConsumer):
             pass
 
 
+class UserConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.user = self.scope["user"]
+        if self.user.is_anonymous:
+            await self.close()
+            return
+
+        self.user_group_name = f"user_{self.user.id}"
+
+        await self.channel_layer.group_add(
+            self.user_group_name,
+            self.channel_name
+        )
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        if hasattr(self, 'user_group_name'):
+            await self.channel_layer.group_discard(
+                self.user_group_name,
+                self.channel_name
+            )
+
+    async def incoming_call(self, event):
+        await self.send(text_data=json.dumps({
+            'type': 'incoming_call',
+            'call': event['call']
+        }))
+
+
 class NotificationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.user = self.scope["user"]
